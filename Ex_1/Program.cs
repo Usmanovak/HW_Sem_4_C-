2f9fa81d82417fc815e68c927737eef5caded353
Karina <usmkarin@gmail.com>
2023-04-07 22:00:10 +0700

    Задача 1

---
+++ Ex_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// ex1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{Math.Pow(a, b)}");
