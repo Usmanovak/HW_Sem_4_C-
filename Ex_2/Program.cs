3c71f55fc38d8845a430bc29bdd76b935cafc54d
Karina <usmkarin@gmail.com>
2023-04-08 11:17:38 +0700

    Задача 2

---
+++ Ex_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// ex 2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int Summa(int n)
+{
+    int result = 0;
+    while (n > 0)
+    {
+        result = result + n % 10;
+        n = n / 10;
+    }
+    return result;
+}
+Console.WriteLine($"Сумма цифр в числе {n} равна {Summa(n)}");
