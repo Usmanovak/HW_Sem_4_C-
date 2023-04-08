1f5ef03ac01800060130548a72bc780f8a403d17
Karina <usmkarin@gmail.com>
2023-04-08 10:52:35 +0700

    Задача 3

---
+++ Ex_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// ex 3. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+int [] array = GetArray(8);
+int [] GetArray (int size) {
+int [] result = new int [size];
+for (int i = 0; i < size; i++) {
+    result[i] = new Random().Next(100);
+}
+return result;
+}
+Console.WriteLine($"[{String.Join(",", array)}]");
